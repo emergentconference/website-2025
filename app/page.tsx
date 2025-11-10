f1c6f00eb464fd6eb249cc2871715b1932133e52
Alex Mazansky <11902347+amazansky@users.noreply.github.com>
2025-11-10 18:43:00 -0500

    start site

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,39 @@
+import gradient from "@/public/gradient.jpg";
+import logoWordmark from "@/public/logo-wordmark.png";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <>
+      <div className="absolute inset-0 -z-10 opacity-50">
+        <Image src={gradient} alt="" fill className="object-cover" />
+      </div>
+      <div className="items-start px-4 mx-auto my-12 max-w-2xl space-y-4 border-black relative">
+        <hr className="border-t-4 border-black" />
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          src={logoWordmark}
+          alt="Emergent"
+          width={2141}
+          height={501}
+          className="w-100"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit ante,
+          lacinia ut sollicitudin id, sodales in eros. Mauris non augue
+          placerat, ornare magna id, volutpat risus. Curabitur maximus, libero
+          sit amet cursus finibus, sem purus pharetra velit, eget pretium risus
+          enim et arcu. Nullam consequat, leo id dictum ultricies, libero quam
+          semper libero, ac vulputate nunc ante eu ante. Aliquam erat volutpat.
+        </p>
+        <p>
+          Etiam euismod velit faucibus ultricies blandit. Maecenas accumsan
+          metus et eros placerat, eget ultricies lectus aliquet. Proin ut tellus
+          fermentum, scelerisque erat in, rutrum tellus. Class aptent taciti
+          sociosqu ad litora torquent per conubia nostra, per inceptos
+          himenaeos. Cras eu magna dui. Vestibulum faucibus tortor sed pretium
+          maximus. Donec bibendum elit et mauris posuere feugiat.
+        </p>
+      </div>
+    </>
   );
 }
